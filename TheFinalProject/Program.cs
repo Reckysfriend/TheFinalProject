e5ac6140e6d04802f8128ebf4c780554eb1b4d76
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2025-01-08 12:23:41 +0100

    Added debug tool
    Added a debug tool for quicker creation of items.

--- TheFinalProject/Program.cs
+++ TheFinalProject/Program.cs
@@ -17,6 +17,8 @@
             */
             Book book = new Book("Lord of the Rings", "One of the best books ever made", Catagory.Book, 10, 100);
             Console.WriteLine(book);
+            Debug debug = new Debug();
+            debug.RunDebug();
         }
     }
 }
