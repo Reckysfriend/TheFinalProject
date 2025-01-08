a38b5416d719d1b18a3eb6dab1091c23504c7716
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2025-01-08 09:59:06 +0100

    Add project files.

---
+++ TheFinalProject/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TheFinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  1.Som shoppare vill jag kunna se olika produkter till försäljning för att när jag hittar det jag är
+                intresserade av, köpa den genom att lägga den till kundvagnen.
+
+                2.Som shoppare vill jag kunna hantera produkter i min kundvagn tex lägg till ta bort och
+                uppdatera.Jag vill se vad jag ska betala totalt för produkterna i kundvagnen.
+
+                3.Som administratör vill jag kunna hantera produktlistan och lägga till, redigera eller ta bort
+                produkter till försäljning från webbplatsen.
+
+                4.Kom på och skriv ner fyra egna user stories som du implementerar i din app.
+            */
+        }
+    }
+}
