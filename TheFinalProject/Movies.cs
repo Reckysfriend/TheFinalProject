2ed1121454e9052c4c5a72627828278f8228e7aa
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2025-01-08 13:33:42 +0100

    Added halv the remaning classes

---
+++ TheFinalProject/Movies.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFinalProject
+{
+    internal class Movies : Item
+    {
+        public string Studio {  get; private set; }
+        public string Producer { get; private set; }
+        public string Director { get; private set; }
+        public AgeRating AgeRating { get; private set; }
+        public string Genre1 { get; private set; }
+        public string Genre2 { get; private set; }
+
+        public Movies(string name, string description, ItemCategory category, int price, int qunatity, string studio, string producer, string director, AgeRating ageRating, string genre1, string genre2)
+           : base(name, description, category, price, qunatity)
+        {
+            Studio = studio;
+            Producer = producer;
+            Director = director;
+            AgeRating = ageRating;
+            Genre1 = genre1;
+            Genre2 = genre2;
+        }
+    }
+   
+    public enum AgeRating
+    {
+        G,
+        PG,
+        PG13,
+        R,
+        NC17
+    }
+}
