2088ad4176779eba8b407847e322b02a033113bf
Viggo Mikaelsson <h24vigmi@du.se>
2025-01-09 10:36:19 +0100

    not finished
    not finished

--- TheFinalProject/ShoppingCart.cs
+++ TheFinalProject/ShoppingCart.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TheFinalProject
 {
     internal class ShoppingCart
     {
+        public List<ShoppingCart> ShoppingCartlist = new List<ShoppingCart> ();
+        ShoppingCart shoppingCart = new ShoppingCart ();
     }
+    public void AddItemToShoppingCart()
+        {
+            ShoppingCart cart = new ShoppingCart ();
+            Item item = new Item();
+            Console.WriteLine("What items would you like to add to your shoppingcart?");
+            Console.WriteLine($"{item}");
+            
+            Console.WriteLine($"you have added{item} to your shoppingcart");
+        }
 }
